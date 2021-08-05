dd93cf6a13d1d213814f01d87615894a5c96b1aa
jhalter <868228+jhalter@users.noreply.github.com>
2021-08-04 19:10:54 -0700

    Update mobius-hotline-server.rb

--- mobius-hotline-server.rb
+++ mobius-hotline-server.rb
@@ -2,7 +2,7 @@ class MobiusHotlineServer < Formula
   desc "Mobius Hotline server"
   homepage "https://github.com/jhalter/mobius"
   url "https://github.com/jhalter/mobius/archive/refs/tags/v0.0.10.tar.gz"
-  sha256 "c33911b12b64dc57ab471bcb5b3af61aad6fdadb0caa61cef39bbcf04fcb34fe"
+  sha256 "cb8e2056eb0175307c73f947db028de078ef87324a22b4cc1e912e761642c4c3"
   license "MIT"
 
   depends_on "go" => :build
